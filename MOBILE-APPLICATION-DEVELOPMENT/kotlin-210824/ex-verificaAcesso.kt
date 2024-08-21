c7cceda169ccc9032cf98551f538b4d3980d2cab
Caio Eduardo <caio3martins@gmail.com>
2024-08-21 20:11:39 -0300

    Atualizando exercicios do dia 21 de agosto de 2024

---
+++ MOBILE-APPLICATION-DEVELOPMENT/kotlin-210824/ex-verificaAcesso.kt
@@ -0,0 +1,18 @@
+// Crie um programa que verifica se o usuário tem acesso ao sistema ou nao
+
+fun main() {
+
+    val usuario = "Vinny"
+    val senha = 123456
+
+    if (usuario == "Vinny" && senha == 123456) {
+
+        println("Acesso permitido.")
+
+    } else {
+
+        println("Acesso negado.")
+        
+    }
+
+}
