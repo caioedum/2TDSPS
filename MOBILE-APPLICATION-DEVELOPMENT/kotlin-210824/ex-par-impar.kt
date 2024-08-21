6e625a172e6c6fec50338ef7581f855ae87f36f8
Caio Eduardo <caio3martins@gmail.com>
2024-08-21 20:06:42 -0300

    Atualizando execercícios do dia 21 de agosto

---
+++ MOBILE-APPLICATION-DEVELOPMENT/kotlin-210824/ex-par-impar.kt
@@ -0,0 +1,16 @@
+// Faça um programa que verifica que o número é par ou ímpar
+
+fun main() {
+
+    val numero = 7
+
+    if (numero % 2 == 0) {
+
+        println("O numero $numero e par")
+
+    } else {
+
+        println("O numero $numero e impar")
+        
+    }
+}
